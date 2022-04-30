5b6fa6b9f52369509cc13b3301327e4d312e2808
PaMarius <marius_maryus1997@yahoo.com>
2022-04-30 14:08:19 +0300

    Update Chess Coders

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use script";
+const openMsgAlert = document.querySelector(".alert");
+const closeBtnAlert = document.querySelector(".close-btn-alert");
+const infoButton = document.querySelector(".info-button");
+const accButton = document.querySelector(".acc-button");
+
+const showAlert = function () {
+  alert("Coming soon");
+};
+
+const closeAlert = function () {
+  openMsgAlert.classList.add("hidden");
+};
+
+const openAlert = function () {
+  openMsgAlert.classList.remove("hidden");
+};
+
+infoButton.addEventListener("click", showAlert);
+accButton.addEventListener("click", openAlert);
+closeBtnAlert.addEventListener("click", closeAlert);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !openMsgAlert.classList.contains("hidden")) {
+    closeAlert();
+  }
+});
